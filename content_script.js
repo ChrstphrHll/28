c0abe67eb34622ab197c9eb3d8ecdffb9263e1ba
Christopher Hall II <christopher.s.hall@duke.edu>
2022-10-27 12:57:51 -0400

    working mvp

--- content_script.js
+++ content_script.js
@@ -42,20 +42,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\b28\b/gi, "<span>28!!!!!!!!!!!!!!!!!!</span>");
-	v = v.replace(/\ballegedly\b/gi, "kinda probably");
-	v = v.replace(/\bnew study\b/gi, "Tumblr post");
-	v = v.replace(/\brebuild\b/gi, "avenge");
-	v = v.replace(/\bspace\b/gi, "spaaace");
-	v = v.replace(/\bgoogle glass\b/gi, "Virtual Boy");
-	v = v.replace(/\bsmartphone\b/gi, "Pokédex");
-	v = v.replace(/\belectric\b/gi, "atomic");
-	v = v.replace(/\bsenator\b/gi, "Elf-Lord");
-	v = v.replace(/\bcar\b/gi, "cat");
-	v = v.replace(/\belection\b/gi, "eating contest");
-	v = v.replace(/\bcongressional leaders\b/gi, "river spirits");
-	v = v.replace(/\bhomeland security\b/gi, "homestar runner");
-	v = v.replace(/\bcould not be reached for comment\b/gi, "is guilty and everyone knows it");
-	
+	v = v.replace(/\b28\b/gi, "28!");
+	v = v.replace(/\b1028\b/gi, "1028!!!!!!");
+
 	textNode.nodeValue = v;
 }
--- popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.sendMessage({type: 'get-28'}, (response) => {
-    // do stuff with response (which will be the value of messageQueue
-    // sent from background.js).
-    document.getElementById("28").innerText = response
-});
